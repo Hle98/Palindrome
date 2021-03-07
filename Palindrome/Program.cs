510b5891628fa6f5472e4e527beba99e1a9de8f9
Huy Le <Lequochuy110898@gmail.com>
2021-03-06 22:23:36 -0500

    first commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("abikba"));
+        }
+        static bool IsPalindrome(string s)
+        {
+            bool result = true;
+            if (s[0] != s[s.Length - 1])
+            {
+                result = false;
+
+            }
+            else if (s.Length > 2)
+            {
+                s = s.Substring(1, s.Length - 2);
+                return IsPalindrome(s);
+            }
+            else
+            {
+                result = true;
+            }
+            if (result)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
+       
+            
+            
+            
+           
+           
+            
+        
+
+
+   
+
+
+       
+
+      
+
+
+    
+
